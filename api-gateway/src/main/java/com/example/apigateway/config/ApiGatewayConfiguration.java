2251b4cbaa0347dc73634f92b04828a59a486333
IvanSlavov <ivan.slavov@nevexis.com>
2023-05-11 17:26:44 +0300

    spring cloud dependency

---
+++ api-gateway/src/main/java/com/example/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(predicate ->
+                    predicate.path("/get")
+                            .filters(f -> f.addRequestHeader("Header", "My URI")
+                                    .addRequestParameter("Param", "Value"))
+                            .uri("http://httpbin.org:80")
+                )
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+
+}
